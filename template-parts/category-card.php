d489e6f856739a5c8512103bf0e5f94d21bd708f
Eduardo Santos <4b4c4t3666@gmail.com>
2025-09-19 18:46:18 -0300

    updates

--- template-parts/category-card.php
+++ template-parts/category-card.php
@@ -21,8 +21,8 @@ for ($i = 1; $i <= 3; $i++) {
     // pega texto do card
     $card_text = get_theme_mod("featured_cat_text_$i", __("Confira os melhores", "gprodemi"));
 ?>
-    <a href="<?php echo esc_url(get_category_link($cat->term_id)); ?>" class="w-full !p-2 lg:p-0 rounded-xl bg-white/5 backdrop-blur-sm flex-1 flex flex-col items-center justify-center border border-gray-300">
-        <h3 class="!text-md !font-medium"><?php echo esc_html($cat->name); ?></h3>
+    <a href="<?php echo esc_url(get_category_link($cat->term_id)); ?>" class="w-full !p-6 lg:p-0 rounded-xl bg-white/5 backdrop-blur-sm flex-1 flex flex-col items-center justify-center border border-gray-300">
+        <h3 class="!text-lg !font-medium"><?php echo esc_html($cat->name); ?></h3>
         <span class="text-gray-600"><?php echo esc_html($card_text); ?></span>
     </a>
 <?php
