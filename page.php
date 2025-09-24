3882e13002be2c8eb48db8662999de56935e9504
Eduardo Santos <4b4c4t3666@gmail.com>
2025-09-24 15:55:55 -0300

    fixed layout page to same the single

--- page.php
+++ page.php
@@ -5,11 +5,19 @@ if (! defined('ABSPATH')) {
 
 get_header(); ?>
 
-<section id="artigo" class="py-8 w-full max-w-4xl mx-auto">
-	<h1 class="!text-3xl font-semibold mb-4"><?php the_title(); ?></h1>
-	<article class="w-full container mx-auto space-y-4 mb-8 text-lg/8 text-gray-600">
+<section id="artigos" class="py-2 md:py-8 w-full max-w-4xl mx-auto">
+	<h1 class="!text-[1.375rem] md:!text-3xl font-semibold mb-4"><?php the_title(); ?></h1>
+	<article id="artigo" class="w-full container mx-auto space-y-4 mb-16">
 		<?php the_content(); ?>
 	</article>
+	<?php
+	wp_link_pages([
+		'before' => '<div class="page-links">Páginas: ',
+		'link_before' => '<span class="page-link">',
+		'link_after' => '</span>',
+		'after'  => '</div>',
+	]);
+	?>
 </section>
 
 <?php get_footer(); ?>
