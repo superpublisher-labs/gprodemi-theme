5cd10ba21910d1b19cb53979da572eb4411a2696
Eduardo Santos <4b4c4t3666@gmail.com>
2025-09-25 09:30:42 -0300

    removed font-awesome

--- functions.php
+++ functions.php
@@ -191,13 +191,6 @@ add_action('wp_enqueue_scripts', function () {
 		filemtime(get_theme_file_path('/assets/js/main.js')),
 		true
 	);
-
-	wp_enqueue_style(
-		'font-awesome',
-		'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css',
-		[],
-		'6.5.2'
-	);
 });
 
 // Setup theme
