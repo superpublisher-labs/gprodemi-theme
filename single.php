1b55cfc42fd3888387dc64b994c0bec110a883f0
Eduardo Santos <80988486+D1ng0ls@users.noreply.github.com>
2026-02-05 13:50:39 -0300

    Fixed post pagination breakdown in single.php by ensuring wp_link_pages() is called inside the main Loop.

--- single.php
+++ single.php
@@ -6,27 +6,36 @@ if (! defined('ABSPATH')) {
 $displayed_posts = [get_the_ID()];
 set_query_var('displayed_posts', $displayed_posts);
 
-get_header(); ?>
+get_header();
+?>
 
 <section id="artigos" class="py-2 md:py-8 w-full max-w-4xl mx-auto">
-	<h1 class="!text-[1.375rem] md:!text-3xl font-semibold mb-4"><?php the_title(); ?></h1>
-	<article id="artigo" class="w-full container mx-auto space-y-4 mb-16">
-		<?php the_content(); ?>
-	</article>
-	<?php
-	wp_link_pages([
-		'before' => '<div class="page-links">Páginas: ',
-		'link_before' => '<span class="page-link">',
-		'link_after' => '</span>',
-		'after'  => '</div>',
-	]);
-	?>
 
-	<?php get_template_part('template-parts/divider'); ?>
 	<?php
 	if (have_posts()) :
 		while (have_posts()) : the_post();
+	?>
+			<h1 class="!text-[1.375rem] md:!text-3xl font-semibold mb-4"><?php the_title(); ?></h1>
+			<article id="artigo" class="w-full container mx-auto space-y-4 mb-16">
+				<?php the_content(); ?>
+			</article>
+
+			<?php
+			wp_link_pages([
+				'before' => '<div class="page-links">Páginas: ',
+				'link_before' => '<span class="page-link">',
+				'link_after' => '</span>',
+				'after'  => '</div>',
+			]);
+			?>
+
+			<?php get_template_part('template-parts/divider'); ?>
+
+			<?php
 			get_template_part('template-parts/authors-card');
+			?>
+
+	<?php
 		endwhile;
 	endif;
 	?>
